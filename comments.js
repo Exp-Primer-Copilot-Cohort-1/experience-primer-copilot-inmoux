fecdf45ba98d243013b522d16450444c958dd8a0
inmoux <142433823+inmoux@users.noreply.github.com>
2024-05-09 10:41:23 +0200

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,36 @@
 // Create web server
-// Import the required modules
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
 
-// Define the hostname and port number
-const hostname = '127.0.0.1';
-const port = 3000;
+// Create web server
+http.createServer(function(request, response) {
+  // Parse the request containing file name
+  var pathname = url.parse(request.url).pathname;
+  // Print the name of the file for which request is made.
+  console.log("Request for " + pathname + " received.");
 
-// Create the HTTP server
-const server = http.createServer((req, res) => {
-  // Set the status code and content type of the response
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  // Read the requested file content from file system
+  fs.readFile(pathname.substr(1), function(err, data) {
+    if (err) {
+      console.log(err);
+      // HTTP Status: 404 : NOT FOUND
+      // Content Type: text/plain
+      response.writeHead(404, {'Content-Type': 'text/html'});
+    } else {
+      // Page found
+      // HTTP Status: 200 : OK
+      // Content Type: text/plain
+      response.writeHead(200, {'Content-Type': 'text/html'});
 
-  // Send the response
-  res.end('Hello, world!\n');
-});
+      // Write the content of the file to response body
+      response.write(data.toString());
+    }
+    // Send the response body
+    response.end();
+  });
+}).listen(8080);
 
-// Start the server and listen for incoming requests
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Print URL for accessing server
+console.log('Server running at http://
